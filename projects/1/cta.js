65506f0c1c7b5c5f04c74717a4c387240d766011
Gordon Greenberg <ggreenberg4@cps.edu>
2017-02-28 15:14:07 +0000

    hook up api

--- projects/1/cta.js
+++ projects/1/cta.js
@@ -2,8 +2,8 @@ $(function() {
 
           var apiPassThruUrl = "https://polar-garden-75406.herokuapp.com/apiPassThru.php";
           
-          var apiEndpoint = "http://ctabustracker.com/bustime/api/v2/getroutes";
-
+          var apiEndpoint = "http://ctabustracker.com/bustime/api/v2/getpredictions";
+          /*global $ */
           
           $.ajax({
               url: apiPassThruUrl,
@@ -11,13 +11,17 @@ $(function() {
               method: 'GET',
               data: {"apiEndpoint": apiEndpoint,
                       "key" : "dYrZ2c4SXpxRwCDhrqns3PehW",
-                      "format":"json"}
+                      "rt": "152,49",
+                      "stpid": "12527,12569,8147,8195",
+                      "format":"json",
+              }
             }).done (function (data) {
               console.log(data);
               $("body").append("<h1>Bus Routes</h1>");
+            
              
-              $.each(data["bustime-response"]["routes"], function(i,v) {
-                  $("body").append("Route #" + v.rt + ", " + v.rtnm + "<br>");
+              $.each(data["bustime-response"]["prd"], function(i,v) {
+                  $("body").append("Route #" + v.rt + ", " + v.stpid + "," +  v.prdctdn + "<br><hr>");
               })
 
             });
